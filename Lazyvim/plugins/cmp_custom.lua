5074256085004b003a75cd9a53cf5666d4422503
Ayres <122650188+Kacaii@users.noreply.github.com>
2024-10-19 08:45:37 -0300

    Update catppuccin theme

--- Lazyvim/plugins/cmp_custom.lua
+++ Lazyvim/plugins/cmp_custom.lua
@@ -2,25 +2,33 @@ return {
   {
     "hrsh7th/nvim-cmp",
     opts = function(_, opts)
+      local mocha = require("catppuccin.palettes").get_palette("mocha")
+
       -- Customize the appearance of the completion window and documentation window
       opts.window = {
         completion = {
           border = "rounded", -- Adds rounded borders to the completion menu
+          winhighlight = "Normal:Pmenu,FloatBorder:FloatBorder,Search:None",
         },
         documentation = {
           border = "rounded", -- Adds rounded borders to the documentation window
+          winhighlight = "Normal:Pmenu,FloatBorder:FloatBorder,Search:None",
         },
       }
+
+      -- Apply the background color from the Catppuccin Mocha palette
+      vim.api.nvim_set_hl(0, "Pmenu", { bg = mocha.base })
+      vim.api.nvim_set_hl(0, "FloatBorder", { bg = mocha.base, fg = mocha.subtext0 })
     end,
   },
   {
-    -- Addind cmp-cmdline
+    -- Adding cmp-cmdline
     "hrsh7th/nvim-cmp",
     dependencies = { "hrsh7th/cmp-cmdline" },
     opts = function(_, opts)
       local cmp = require("cmp")
 
-      table.insert(opts.sources, { name = "hrsh7th/cmp-cmdline" })
+      table.insert(opts.sources, { name = "cmdline" })
 
       cmp.setup.cmdline({ "/", "?" }, {
         mapping = cmp.mapping.preset.cmdline(),
