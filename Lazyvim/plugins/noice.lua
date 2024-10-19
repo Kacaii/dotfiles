4be7e7a7dc41bc798d61309827846f2f33ad0244
Ayres <122650188+Kacaii@users.noreply.github.com>
2024-10-18 21:18:54 -0300

    Custom Icons

--- Lazyvim/plugins/noice.lua
+++ Lazyvim/plugins/noice.lua
@@ -4,13 +4,14 @@ return {
     "folke/noice.nvim",
     opts = {
       presets = {
+        bottom_search = false,
         lsp_doc_border = true, -- add a border to hover docs and signature help
       },
     },
   },
 
   {
-    -- Disabling annoying message
+    -- Disabling "No information available" notification
     "folke/noice.nvim",
     opts = function(_, opts)
       table.insert(opts.routes, {
@@ -20,6 +21,29 @@ return {
         },
         opts = { skip = true },
       })
+    end,
+  },
+
+  {
+    "folke/noice.nvim",
+    opts = function(_, opts)
+      opts.cmdline = {
+        format = {
+          -- conceal: (default=true) This will hide the text in the cmdline that matches the pattern.
+          -- view: (default is cmdline view)
+          -- opts: any options passed to the view
+          -- icon_hl_group: optional hl_group for the icon
+          -- title: set to anything or empty string to hide
+          cmdline = { pattern = "^:", icon = "", lang = "vim" },
+          search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
+          search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
+          filter = { pattern = "^:%s*!", icon = "", lang = "bash" },
+          lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
+          help = { pattern = "^:%s*he?l?p?%s+", icon = "󰞋" },
+          input = { view = "cmdline_input", icon = "󰥻 " }, -- Used by input()
+          -- lua = false, -- to disable a format, set to `false`
+        },
+      }
     end,
   },
 }
