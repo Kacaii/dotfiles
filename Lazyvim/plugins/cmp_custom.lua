abff41ff5debbe7c4f616d44578860c36e08f683
Ayres <122650188+Kacaii@users.noreply.github.com>
2024-10-19 08:05:49 -0300

    add cmp-cmdline

--- Lazyvim/plugins/cmp_custom.lua
+++ Lazyvim/plugins/cmp_custom.lua
@@ -1,16 +1,43 @@
--- File: lua/plugins/cmp_custom.lua
-
 return {
-  "hrsh7th/nvim-cmp",
-  opts = function(_, opts)
-    -- Customize the appearance of the completion window and documentation window
-    opts.window = {
-      completion = {
-        border = "rounded", -- Adds rounded borders to the completion menu
-      },
-      documentation = {
-        border = "rounded", -- Adds rounded borders to the documentation window
-      },
-    }
-  end,
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      -- Customize the appearance of the completion window and documentation window
+      opts.window = {
+        completion = {
+          border = "rounded", -- Adds rounded borders to the completion menu
+        },
+        documentation = {
+          border = "rounded", -- Adds rounded borders to the documentation window
+        },
+      }
+    end,
+  },
+  {
+    -- Addind cmp-cmdline
+    "hrsh7th/nvim-cmp",
+    dependencies = { "hrsh7th/cmp-cmdline" },
+    opts = function(_, opts)
+      local cmp = require("cmp")
+
+      table.insert(opts.sources, { name = "hrsh7th/cmp-cmdline" })
+
+      cmp.setup.cmdline({ "/", "?" }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" },
+        },
+      })
+
+      -- Setup for `:` to use cmdline and path sources
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          { name = "cmdline" },
+        }),
+      })
+    end,
+  },
 }
