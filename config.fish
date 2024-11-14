56c2af4087d57c5855e2478175c448bbfa3c7366
Ayres <122650188+Kacaii@users.noreply.github.com>
2024-11-13 21:34:23 -0300

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,6 +1,8 @@
 if status is-interactive
 
-    # Commands to run in interactive sessions can go here
+    # Disables fish_greeting 
+    set -g fish_greeting ""
+    set -gx TERM xterm-256color
 
     # Adding stuff to Path  
     fish_add_path /snap/bin
@@ -14,14 +16,18 @@ if status is-interactive
     abbr cls clear
     abbr v nvim
     abbr vv nvim .
+    abbr fvv 'nvim $(fzf)'
     abbr g git
-    abbr gc git clone
+    abbr gp git pull
+    abbr gc ghq get
     abbr gco git checkout
     abbr ls lsd
     abbr ll lsd -l
     abbr lla lsd -la
     abbr llt lsd --tree --depth 2
     abbr tk tmux kill-server
+    abbr pbat 'prettybat --color=always --theme="Catppuccin Mocha"'
+    abbr tt tldr
 
     # Adding alias
     alias uu='sudo apt update -y && sudo apt full-upgrade -y && brew upgrade' # Upgrade Packages and Updates Package Panager
@@ -30,25 +36,18 @@ if status is-interactive
     alias fcg='nvim ~/.config/fish/config.fish'
     alias tcg='nvim ~/.tmux.conf'
     alias fzf='fzf --tmux 65% --preview "prettybat --style=numbers --color=always {}"'
-    alias bat='prettybat --color=always --theme="Catppuccin Mocha"'
 
-    # Disables fish_greeting 
-    set -g fish_greeting
+    set -gx EDITOR nvim
 
-    set BAT_THEME "Catppuccin Mocha"
-
-    # Colored MAN Config  
+    # Colored MAN Config ' 
     set -g man_bold -o cba7f7
 
-    # Sets fzf file search to < Ctrl + F > and fzf variables search to < Ctrl + Alt + V >
-    fzf_configure_bindings --directory=\cf --variables=\e\cv
-
-    # Set up fzf key bindings
-    # fzf --fish | source
+    # Set up fzf integration
+    fzf --fish | source
 
     # Set up fzf colors
-    set -Ux FZF_DEFAULT_OPTS "\
-        --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+    set -Ux FZF_DEFAULT_OPTS " \
+                --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
         --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
         --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
         --color=selected-bg:#45475a \
