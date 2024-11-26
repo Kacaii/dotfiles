830b2fdf3cf1950d8807b09d4e7c12bc1be80dfd
Kacaii <kacaii.privado@gmail.com>
2024-11-26 20:16:24 -0300

    add ts snippets

--- nvim/plugins/snippets.lua
+++ nvim/plugins/snippets.lua
@@ -1,35 +1,73 @@
 return {
-  "L3MON4D3/LuaSnip",
-  opts = function()
-    local ls = require("luasnip")
-    local s = ls.snippet
-    local i = ls.insert_node
-    local t = ls.text_node
+  {
+    "L3MON4D3/LuaSnip",
+    opts = function()
+      local ls = require("luasnip")
+      local s = ls.snippet
+      local i = ls.insert_node
+      local t = ls.text_node
 
-    ls.add_snippets("javascript", {
+      ls.add_snippets("javascript", {
 
-      s({ trig = "jsd", desc = "Javascript Documentation  " }, {
-        t({ "  /**", "  * " }),
-        i(1),
-        t({ "", "  * ", "  * " }),
-        i(2),
-        t({ "", "  */ " }),
-      }),
-      s({ trig = "ijsd", desc = "Inline Javascript Documentation  " }, {
-        t({ "  /** " }),
-        i(1),
-        t({ " */" }),
-      }),
-      s({ trig = "cdb", desc = "Add CodeBlock   " }, {
-        t({ "```" }),
-        i(1),
-        t({ "", "* " }),
-        i(2),
-        t({ "", "* ```" }),
-      }),
-      s({ trig = "cc", desc = "Clear the Console 󰆍 " }, {
-        t("console.clear()"),
-      }),
-    })
-  end,
+        s({ trig = "jsd", desc = "Javascript Documentation  " }, {
+          t({ "  /**", "  * " }),
+          i(1),
+          t({ "", "  * ", "  * " }),
+          i(2),
+          t({ "", "  */ " }),
+        }),
+        s({ trig = "ijsd", desc = "Inline Javascript Documentation  " }, {
+          t({ "  /** " }),
+          i(1),
+          t({ " */" }),
+        }),
+        s({ trig = "cdb", desc = "Add CodeBlock   " }, {
+          t({ "```" }),
+          i(1),
+          t({ "", "* " }),
+          i(2),
+          t({ "", "* ```" }),
+        }),
+        s({ trig = "cc", desc = "Clear the Console 󰆍 " }, {
+          t("console.clear()"),
+        }),
+      })
+    end,
+  },
+  {
+
+    "L3MON4D3/LuaSnip",
+    opts = function()
+      local ls = require("luasnip")
+      local s = ls.snippet
+      local i = ls.insert_node
+      local t = ls.text_node
+
+      ls.add_snippets("typescript", {
+
+        s({ trig = "jsd", desc = "TypeScript Documentation  " }, {
+          t({ "  /**", "  * " }),
+          i(1),
+          t({ "", "  * ", "  * " }),
+          i(2),
+          t({ "", "  */ " }),
+        }),
+        s({ trig = "ijsd", desc = "Inline Typescript Documentation  " }, {
+          t({ "  /** " }),
+          i(1),
+          t({ " */" }),
+        }),
+        s({ trig = "cdb", desc = "Add CodeBlock   " }, {
+          t({ "```" }),
+          i(1),
+          t({ "", "* " }),
+          i(2),
+          t({ "", "* ```" }),
+        }),
+        s({ trig = "cc", desc = "Clear the Console 󰆍 " }, {
+          t("console.clear()"),
+        }),
+      })
+    end,
+  },
 }
