42841bf64218ed68a45cc8f92f67e228a4ce525a
Kacaii <kacaii.privado@gmail.com>
2025-02-10 17:05:23 -0300

    update fish config

--- config.fish
+++ config.fish
@@ -1,56 +1,26 @@
 if status is-interactive
 
-    # Disables fish_greeting 
-    set -g fish_greeting ""
+    set -g fish_greeting "" # Disables fish_greeting 
     set -gx TERM xterm-256color
-    set -gx EDITOR nvim # Set EDITOR to nvim - 
+
+    set -gx EDITOR nvim
+    abbr v nvim # 
+
+    # Adding stuff to Path 
+    fish_add_path /home/kacaii/.bun/bin
+    fish_add_path /home/kacaii/.cargo/bin
+    fish_add_path /home/kacaii/.local/bin
+    fish_add_path /home/kacaii/custom-scripts/
+    fish_add_path /home/kacaii/go/bin/
+    fish_add_path /home/linuxbrew/.linuxbrew/bin
+    fish_add_path /home/linuxbrew/.linuxbrew/sbin
+    fish_add_path /snap/bin
 
     # Colored MAN Config 
     set -g man_bold -o cba7f7
 
     # Set up FZF integration 
     fzf --fish | source
-
-    # Adding stuff to Path 
-    fish_add_path /snap/bin
-    fish_add_path /home/kacaii/custom-scripts/
-    fish_add_path /home/kacaii/.bun/bin
-    fish_add_path /home/linuxbrew/.linuxbrew/bin
-    fish_add_path /home/linuxbrew/.linuxbrew/sbin
-    fish_add_path /home/kacaii/.cargo/bin
-    fish_add_path /home/kacaii/.local/bin
-    fish_add_path /home/kacaii/go/bin/
-
-    # Adding abbreviations
-    abbr cls clear # Clear Screen - 
-    abbr v nvim # Open nvim - 
-    # abbr vv "nvim ." # Open current directory in neovim
-    abbr fvv 'nvim $(fzf)' # Open file in neovim using fzf
-    abbr g git # Git commands - 󰊢
-    abbr gg lazygit # Open lazygit GUI - 󰊢
-    abbr gp "git pull" # Pull - 󰊢
-    abbr gc "ghq get" # Clone repository - 󰊢
-    abbr tk "tmux kill-server" # Kill tmux server
-    abbr pbat 'prettybat --color=always --theme="Catppuccin Mocha"' # Pretty Bat using Catppuccin Mocha theme
-    abbr tt tmux # Shortcut to tmux 
-    abbr fcc fish_clipboard_copy # Copy to clipboard
-    abbr zbr 'zig build run' # 
-
-    # Upgrade Packages and Updates Package Panager
-    alias uu='\
-      sudo apt update -y \
-      && sudo apt full-upgrade -y \
-      && brew upgrade \
-      && ya pack -u'
-
-    alias update_dotfiles='\
-  cp -r ~/.config/nvim/lua/ $(ghq root)/github.com/Kacaii/dotfiles/nvim/ \
-  && cp -r ~/.config/fish/config.fish $(ghq root)/github.com/Kacaii/dotfiles/ \
-  '
-
-    alias fcg='nvim ~/.config/fish/config.fish' # Open config.fish in neovim
-    alias tcg='nvim ~/.tmux.conf' # Open tmux.conf in neovim
-    # alias fzf='fzf  --preview "prettybat --style=numbers --color=always {}"'
 
     # Set up FZF colors
     set -Ux FZF_DEFAULT_OPTS "\
@@ -60,35 +30,60 @@ if status is-interactive
       --color=selected-bg:#45475a \
       --multi"
 
-    function yy # Yazi Q setup 󰇥
+    abbr cls clear # Clear Screen 
+    abbr fcc fish_clipboard_copy # Copy to clipboard
 
+    abbr pbat 'prettybat --color=always --theme="Catppuccin Mocha"' # Pretty Bat using Catppuccin Mocha theme
+
+    #  Git abreviations
+    abbr g git
+    abbr gc "ghq get"
+    abbr gg lazygit
+    abbr gp "git pull"
+
+    #  Zig Abbreviations
+    abbr zbr 'zig build run'
+    abbr zbt 'zig build test'
+
+    #  Tmux abbreviations
+    abbr tk "tmux kill-server"
+    abbr tt tmux
+
+    # Upgrade Packages and Updates Package Panager
+    function uu
+        sudo apt update -y
+        sudo apt full-upgrade -y
+        brew upgrade
+        ya pack -u
+        fisher update
+    end
+
+    # Copies nvim configuration
+    function update_dotfiles
+        cp -r ~/.config/nvim/lua/ $(ghq root)/github.com/Kacaii/dotfiles/nvim/
+        cp -r ~/.config/fish/config.fish $(ghq root)/github.com/Kacaii/dotfiles/
+    end
+
+    alias fcg='nvim ~/.config/fish/config.fish' # Open config.fish in neovim
+    alias tcg='nvim ~/.tmux.conf' # Open tmux.conf in neovim
+
+    function yy # Yazi Q setup 󰇥
         set tmp (mktemp -t "yazi-cwd.XXXXXX")
         yazi $argv --cwd-file="$tmp"
         if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
             cd -- "$cwd"
         end
         rm -f -- "$tmp"
-
-    end
-
-    # Prints all files in current directory in pretty bat format
-    function bat_files
-
-        for file in *
-            if test -f $file
-                bat -P --color=always --theme="Catppuccin Mocha" $file
-            end
-        end
-
     end
 
     # For when you need to setup everything quickly
     # For that  you'll need:
-    #
+
     # - Git            https://git-scm.com/downloads/linux
     # - Homebrew       https://brew.sh/
     # - Fish Shell     https://fishshell.com/
     function basic_custom_setup
+
         # Add essential software here
         set ensure_installed \
             bat \
@@ -115,11 +110,10 @@ if status is-interactive
             zig
 
         for formulae in $ensure_installed
-            # brew install $formulae
-            echo $formulae 
+            brew install $formulae
         end # End `for loop`
 
-        fish_update_completions # 
+        fish_update_completions
         echo \n▒▒▒ All done!
     end
 end
