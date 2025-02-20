ee49cd0b9b928c8e1e4042b06233ddd22c0ea5fd
kacaii <kacaii.privado@gmail.com>
2025-02-20 08:25:18 -0300

    revert basic_setup script removal

--- config.fish
+++ config.fish
@@ -58,6 +58,48 @@ if status is-interactive
         fisher update
     end
 
+    # For when you need to setup everything quickly
+    # For that  you'll need:
+
+    # - Git            https://git-scm.com/downloads/linux
+    # - Homebrew       https://brew.sh/
+    # - Fish Shell     https://fishshell.com/
+    function basic_custom_setup
+        # Add essential software here
+        set ensure_installed \
+            bat \
+            bat-extras \
+            deno \
+            docker \
+            fd \
+            fzf \
+            gh \
+            ghq \
+            go \
+            httpie \
+            lazydocker \
+            lazygit \
+            lua \
+            luarocks \
+            neovim \
+            node \
+            php \
+            ripgrep \
+            sqlite \
+            tmux \
+            tree \
+            tree-sitter \
+            xclip \
+            yazi \
+            zig
+        for formulae in $ensure_installed
+            brew install $formulae
+        end
+
+        fish_update_completions
+        echo \n▒▒▒ All done!
+    end
+
     # Copies nvim configuration
     function update_backup_dotfiles
         cp -r ~/.config/fish/config.fish $(ghq root)/github.com/Kacaii/dotfiles/
