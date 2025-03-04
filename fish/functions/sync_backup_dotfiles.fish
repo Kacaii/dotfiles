58a396b3f50b0ed304bb9d822f2517fe0cc682fa
Kacaii <kacaii.privado@gmail.com>
2025-03-03 23:32:28 -0300

    set an list of function files

--- fish/functions/sync_backup_dotfiles.fish
+++ fish/functions/sync_backup_dotfiles.fish
@@ -6,10 +6,16 @@ function sync_backup_dotfiles -d "Backup your current config files 󰕒 "
     cp -r ~/.config/yazi/theme.toml $ghq_dotfiles_root/yazi
     cp -r ~/.tmux.conf $ghq_dotfiles_root/tmux
 
-    cp -r $__fish_config_dir/functions/sync_backup_dotfiles.fish $ghq_dotfiles_root/fish/functions/
-    cp -r $__fish_config_dir/functions/sync_current_dotfiles.fish $ghq_dotfiles_root/fish/functions/
-    cp -r $__fish_config_dir/functions/uu.fish $ghq_dotfiles_root/fish/functions/
-    cp -r $__fish_config_dir/functions/basic_custom_setup.fish $ghq_dotfiles_root/fish/functions/
-    cp -r $__fish_config_dir/functions/fish_prompt.fish $ghq_dotfiles_root/fish/functions/
-    cp -r $__fish_config_dir/functions/fish_right_prompt.fish $ghq_dotfiles_root/fish/functions/
+    set -l custom_fish_functions \
+        sync_backup_dotfiles.fish \
+        sync_current_dotfiles.fish \
+        uu.fish \
+        basic_custom_setup.fish \
+        fish_prompt.fish \
+        fish_right_prompt.fish
+
+    for fn in $custom_fish_functions
+        cp -r $__fish_config_dir/functions/$fn $ghq_dotfiles_root/fish/functions/
+    end
+
 end
--- fish/functions/sync_current_dotfiles.fish
+++ fish/functions/sync_current_dotfiles.fish
@@ -6,10 +6,16 @@ function sync_current_dotfiles -d "Update your current config files 󰇚 "
     cp -r $ghq_dotfiles_root/nvim/snippets/ ~/.config/nvim/
     cp -r $ghq_dotfiles_root/yazi/theme.toml ~/.config/yazi/
 
-    cp -r $ghq_dotfiles_root/fish/functions/sync_backup_dotfiles.fish $__fish_config_dir/functions/
-    cp -r $ghq_dotfiles_root/fish/functions/sync_current_dotfiles.fish $__fish_config_dir/functions/
-    cp -r $ghq_dotfiles_root/fish/functions/uu.fish $__fish_config_dir/functions/
-    cp -r $ghq_dotfiles_root/fish/functions/basic_custom_setup.fish $__fish_config_dir/functions/
-    cp -r $ghq_dotfiles_root/fish/functions/fish_prompt.fish $__fish_config_dir/functions/
-    cp -r $ghq_dotfiles_root/fish/functions/fish_right_prompt.fish $__fish_config_dir/functions/
+    set -l custom_fish_functions \
+        sync_backup_dotfiles.fish \
+        sync_current_dotfiles.fish \
+        uu.fish \
+        basic_custom_setup.fish \
+        fish_prompt.fish \
+        fish_right_prompt.fish
+
+    for fn in $custom_fish_functions
+        cp -r $ghq_dotfiles_root/fish/functions/$fn $__fish_config_dir/functions/
+    end
+
 end
