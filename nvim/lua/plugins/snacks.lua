688ab21f0b5ba820432a1d5e4698a3e8989f6079
Kacaii <kacaii.privado@gmail.com>
2025-03-14 13:14:08 -0300

    change box_shape on dashboard

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -11,14 +11,14 @@ return {
       \ \_\ \_\\ \____\\ \____/ \ \___/  \ \_\\ \_\ \_\ \_\
       \/_/\/_/ \/____/ \/___/   \/__/    \/_/ \/_/\/_/\/_/
 
-┌───────────────────────────────────────────────────────────────────┐
-│ SEG        | TER          | QUA        | QUIN         | SEX       │
-├───────────────────────────────────────────────────────────────────┤
-│ Legislacao | RPD          | Eng. Req   | PTS          | Coding II │
-│ Legislacao | RPD          | Eng. Req   | PTS          | Coding II │
-│ Banco II   | Tech English | Legislacao | Criatividade | Coding II │
-│ Banco II   | Tech English | Legislacao | Criatividade | Coding II │
-└───────────────────────────────────────────────────────────────────┘
+┌────────────┬──────────────┬────────────┬──────────────┬───────────┐
+│ SEG        │ TER          │ QUA        │ QUIN         │ SEX       │
+├────────────┼──────────────┼────────────┼──────────────┼───────────┤
+│ Legislacao │ RPD          │ Eng. Req   │ PTS          │ Coding II │
+│ Legislacao │ RPD          │ Eng. Req   │ PTS          │ Coding II │
+│ Banco II   │ Tech English │ Legislacao │ Criatividade │ Coding II │
+│ Banco II   │ Tech English │ Legislacao │ Criatividade │ Coding II │
+└────────────┴──────────────┴────────────┴──────────────┴───────────┘
 ]],
       },
     },
