ec98f9485aacac0fce1fc695f29897e6e03ae096
Kacaii <kacaii.privado@gmail.com>
2025-04-14 18:51:33 -0300

    move variables to outside of the main if statement

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,17 @@
-# @fish-lsp-disable 4004
-if status is-interactive
+# Disabling Fish Greeting
+set -g fish_greeting ""
 
+set -gx EDITOR nvim
+set -gx TERM xterm-256color
+
+set -gx FZF_DEFAULT_OPTS "\
+      --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+      --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+      --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
+      --color=selected-bg:#45475a \
+      --multi"
+
+if status is-interactive
     set -l ensure_fish_add_path \
         /home/kacaii/.bun/bin \
         /home/kacaii/.cargo/bin \
@@ -14,9 +25,6 @@ if status is-interactive
     for required_path in $ensure_fish_add_path
         fish_add_path $required_path
     end
-
-    # Disabling Fish Greeting
-    set -g fish_greeting ""
 
     #  Edit Fish Config Files 
     bind ctrl-u "\
@@ -33,16 +41,6 @@ if status is-interactive
     set -g __fish_git_prompt_color_dirtystate yellow
     set -g __fish_git_prompt_char_dirtystate 
 
-    set -gx EDITOR nvim
-    set -gx TERM xterm-256color
-
-    set -gx FZF_DEFAULT_OPTS "\
-      --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
-      --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
-      --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
-      --color=selected-bg:#45475a \
-      --multi"
-
     # Setup Shell integrations
     fzf --fish | source
     zoxide init fish | source
@@ -53,6 +51,7 @@ if status is-interactive
     abbr fcc fish_clipboard_copy
     abbr man batman
     abbr v nvim
+    abbr lla "ll -a"
 
     #  Git Abreviations
     abbr g git
@@ -61,15 +60,15 @@ if status is-interactive
     abbr gp "git pull"
 
     #  Zig Abbreviations
-    abbr zbr 'zig build run'
-    abbr zbt 'zig build test --summary all'
+    abbr zbr "zig build run"
+    abbr zbt "zig build test --summary all"
 
     #  Tmux Abbreviations
     abbr tk "tmux kill-server"
     abbr tm tmux
 
     #  Edit config files
-    abbr fcg 'nvim ~/.config/fish/config.fish' # Open config.fish in neovim
-    abbr tcg 'nvim ~/.tmux.conf' # Open tmux.conf in neovim
+    abbr fcg "nvim ~/.config/fish/config.fish" # Open config.fish in neovim
+    abbr tcg "nvim ~/.tmux.conf" # Open tmux.conf in neovim
 
 end
